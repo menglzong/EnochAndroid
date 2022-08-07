9cb654af193b7f7de24162a18be28e26dfbfb3fc
EnochMeng <menglzong@126.com>
2022-08-07 20:23:58 +0800

    rename

--- app/src/main/java/com/example/myapplication/LinearLayoutActivity.kt
+++ app/src/main/java/com/example/myapplication/LinearLayoutActivity.kt
@@ -3,9 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class RelativeLayout : AppCompatActivity() {
+class LinearLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_relative_layout)
+        setContentView(R.layout.activity_linear_layout)
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,17 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    var mBtnLinear: Button? = null
+    var mBtnReal: Button? = null
     var mBtnTextView: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mBtnLinear = findViewById(R.id.btn_linear)
+        mBtnLinear?.setOnClickListener {
+            val inter = Intent(this, LinearLayoutActivity::class.java)
+            startActivity(inter)
+        }
+        mBtnReal = findViewById(R.id.btn_rela)
+        mBtnReal?.setOnClickListener {
+            val inter = Intent(this, RelativeLayoutActivity::class.java)
+            startActivity(inter)
+        }
         mBtnTextView = findViewById(R.id.btn_tv)
-        mBtnTextView?.setOnClickListener(View.OnClickListener { v ->
+        mBtnTextView?.setOnClickListener(View.OnClickListener {
             val inter = Intent(this, TextViewActivity::class.java)
             startActivity(inter)
         })
