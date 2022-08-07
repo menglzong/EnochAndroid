e08c2e90a4f24feffdaf2a808a90696e52fe191b
EnochMeng <menglzong@126.com>
2022-08-07 17:45:43 +0800

    textview

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var mBtnTextView: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mBtnTextView = findViewById(R.id.btn_tv)
+        mBtnTextView?.setOnClickListener(View.OnClickListener { v ->
+            val inter = Intent(this, TextViewActivity().javaClass)
+            startActivity(inter)
+        })
     }
 }
